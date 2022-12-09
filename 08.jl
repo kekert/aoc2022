84b9d302fbda722e83e8aef92afbfb1411f798e0
Jan Simek <44477033+kekert@users.noreply.github.com>
2022-12-09 14:43:27 +0100

    Update 08.jl

--- 08.jl
+++ 08.jl
@@ -2,10 +2,9 @@ input = parse.(Int64, reduce(hcat, collect.(readlines("julia/2022/input/08.txt")
 
 a = b = 0
 for x in axes(input, 1), y in axes(input, 2)
-    val = input[x, y]
     lrtd = [input[x-1:-1:1, y:y], input[x+1:end, y:y], input[x:x, y-1:-1:1], input[x:x, y+1:end]]
     a += any(val .> maximum.(lrtd, init=-1))
-    b = max(b, prod([something(findfirst(i -> i >= val, vec(j)), length(j)) for j in lrtd]))
+    b = max(b, prod([something(findfirst(i -> i >= input[x, y], vec(j)), length(j)) for j in lrtd]))
 end
 
 @show(a, b)
